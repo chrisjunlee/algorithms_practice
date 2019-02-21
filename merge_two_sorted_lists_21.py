feea8cd71bd801dfdfbd6ab8d527b2d955adbba0
Chris <chrisjunlee@gmail.com>
2019-02-20 21:14:28 -0800

    Elegant dummy node solution. Simplifies logic around assigning node.next

--- merge_two_sorted_lists_21.py
+++ merge_two_sorted_lists_21.py
@@ -6,39 +6,20 @@
 
 class Solution:
     def mergeTwoLists(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
-        res = None
-        res_head = res
-        
-        # pointers to each list element
-        min_node = None
+        res = ListNode(0)
+        dummy = res
         
         while l1 and l2:        
-            # compare pointers. take min
             if l1.val < l2.val:
-                min_node = ListNode(l1.val)
+                res.next = l1
                 l1 = l1.next
             else:
-                min_node = ListNode(l2.val)
-                l2 = l2.next
-        
-            # append minimum to res
-            if res is None:
-                res = min_node
-            else:
-                res.next = min_node
-                res = res.next         
-        
-        # append remaining list items from list1 or list2
-        if l1:
-            if res is None:
-                res = l1
-            else:
-                res.next = l1
-        
-        if l2:
-            if res is None:
-                res = l2
-            else:
                 res.next = l2
+                l2 = l2.next
+                
+            res = res.next
+            
+        # only one or none can be non-empty
+        res.next = l1 if l1 else l2
         
-        return res_head
+        return dummy.next
