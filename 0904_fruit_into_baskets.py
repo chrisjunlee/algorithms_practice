439126444ca70031ceabe14a2359a8ee262d49f7
Chris <chrisjunlee@gmail.com>
2019-02-21 02:16:24 -0800

    Result of perf test: keeping dictionary memory footprint low results in 10 fold decrease in speed!

--- 0904_fruit_into_baskets.py
+++ 0904_fruit_into_baskets.py
@@ -1,20 +1,63 @@
+from test_data_0904 import long_test_list
+import sys
+
 from collections import defaultdict
-class Solution:
-    def totalFruit(self, tree: 'List[int]') -> 'int':
-        length, max_length = 0, 0
-        tree_count = defaultdict(lambda: 0)
-        uniq_count = 0
-        left = 0
-        
-        for right, fruit in enumerate(tree):
-            tree_count[fruit] += 1
-            uniq_count = len([v for v in tree_count.values() if v > 0])
-            
-            if uniq_count <= 2:
-                length += 1
-                max_length = max(max_length, length)
-            else:
-                tree_count[tree[left]] -= 1
-                left += 1
-        
-        return max_length
+def totalFruit(tree: 'List[int]') -> 'int':
+    length, max_length = 0, 0
+    tree_count = defaultdict(lambda: 0)
+    uniq_count = 0
+    left = 0
+
+    max_mem = 0
+    for right, fruit in enumerate(tree):
+        max_mem = max(max_mem, sys.getsizeof(tree_count))
+        tree_count[fruit] += 1
+        uniq_count = len([v for v in tree_count.values() if v > 0])
+
+        if uniq_count <= 2:
+            length += 1
+            max_length = max(max_length, length)
+        else:
+            tree_count[tree[left]] -= 1
+            left += 1
+
+    print(max_mem)
+    return max_length
+
+def totalFruit_with_delete(tree: 'List[int]') -> 'int':
+    length, max_length = 0, 0
+    tree_count = defaultdict(lambda: 0)
+    uniq_count = 0
+    left = 0
+
+    max_mem = 0
+    for right, fruit in enumerate(tree):
+        max_mem = max(max_mem, sys.getsizeof(tree_count))
+        print(sys.getsizeof(tree_count))
+        tree_count[fruit] += 1
+        uniq_count = len([v for v in tree_count.values() if v > 0])
+
+        if uniq_count <= 2:
+            length += 1
+            max_length = max(max_length, length)
+        else:
+            tree_count[tree[left]] -= 1
+            if tree_count[tree[left]] == 0:
+                del tree_count[tree[left]]
+            left += 1
+    print(max_mem)
+    return max_length
+
+import timeit
+print(timeit.timeit("totalFruit(long_test_list)", number=5, setup="from __main__ import totalFruit, long_test_list"))
+print(timeit.timeit("totalFruit_2(long_test_list)", number=5, setup="from __main__ import totalFruit_2, long_test_list"))
+
+
+
+
+
+
+
+
+
+
