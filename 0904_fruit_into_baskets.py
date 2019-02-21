0850e9aea2bb0d8c1984b1658000e6977998b804
Chris <chrisjunlee@gmail.com>
2019-02-21 01:01:28 -0800

    Enumeration version: 10x less bug prone, runs much faster

--- 0904_fruit_into_baskets.py
+++ 0904_fruit_into_baskets.py
@@ -1,35 +1,20 @@
-# This version times out. Runtime 306ms vs next iteration's 108ms
-
+from collections import defaultdict
 class Solution:
     def totalFruit(self, tree: 'List[int]') -> 'int':
-        # longest contiguous subarray composed of 2 unique elements
-        left, right = 0, 0
-        tree_count = dict()
-        unique_count = 0
         length, max_length = 0, 0
+        tree_count = defaultdict(lambda: 0)
+        uniq_count = 0
+        left = 0
         
-        if tree:
-            tree_count[tree[0]] = 1
-            length += 1
+        for right, fruit in enumerate(tree):
+            tree_count[fruit] += 1
+            uniq_count = len([v for v in tree_count.values() if v > 0])
             
-        if len(tree) <= 2:
-            return len(tree)
-        
-        # sliding window
-        while left <= right:
-            unique_count = [v > 0 for k, v in tree_count.items()].count(True)
-            
-            if unique_count <= 2:
-                length = right - left + 1
+            if uniq_count <= 2:
+                length += 1
                 max_length = max(max_length, length)
-                
-                if right == len(tree) - 1:
-                    break;
-                    
-                right += 1
-                tree_count[tree[right]] = tree_count.get(tree[right], 0) + 1
-            else:                
-                tree_count[tree[left]] = tree_count.get(tree[left], 0) - 1
+            else:
+                tree_count[tree[left]] -= 1
                 left += 1
-                length -= 1
+        
         return max_length
